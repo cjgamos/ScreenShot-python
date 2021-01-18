c932ba068f0002c4c5c0e4cf203e1eb9940f4bf9
Gamos,CJ C <68680586+cjgamos@users.noreply.github.com>
2021-01-18 22:29:08 +0800

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,10 +1,11 @@
 import pyautogui
 import time
+import tkinter as tk
 
 def screenshot():
 
     name = int(round(time.time() * 10000))
-    name = '{}.png'.format(name)
+    name = 'D:\Repo\Python\ScreenShot-python\data/{}.png'.format(name)
 
 
     time.sleep(5)
@@ -12,4 +13,22 @@ def screenshot():
     img.show()
 
     
-screenshot()
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(
+    frame,
+    text='Take Screenshot',
+    command=screenshot
+)
+
+button.pack(side=tk.LEFT)
+close = tk.Button(
+    frame,
+    text='Quit',
+    command=quit
+)
+close.pack(side=tk.LEFT)
+
+root.mainloop()
